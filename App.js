c28551617ab493e428c687734bae5840861781bd
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-04-09 18:06:33 +0200

    authentication with clerk Oauth

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import Login from "./App/Screens/LoginScreen/Login";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Constants from "expo-constants";
+import * as SecureStore from "expo-secure-store";
+ 
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+    tokenCache={tokenCache}
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+    >
+      <SafeAreaView style={styles.container}>
+        {/* Sign In Component */}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        {/* Sign Out Component */}
+<SignedOut>
+          <Login />
+          </SignedOut>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 20,
   },
 });
